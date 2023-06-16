31c248e81062cf227eba8cca6591d98c0546e115
LucasLucky1 <lucaserickson100@gmail.com>
2023-06-16 20:24:40 -0300

    adding blur and width control on input

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+//capturando os elementos no DOM
 let corInput= document.querySelector('#cor');
-let background =  document.querySelector('.container-img');
+let nivelBlur= document.querySelector('#blur');
+let nivelSize = document.querySelector('#size');
+console.log(nivelBlur,nivelSize)
 
-corInput.addEventListener('change', (e)=> {
-    background.style.backgroundColor =  corInput.value
+//capturando a variável css através do ':root'
+const root = document.querySelector(':root');
+
+
+//mudando a cor do fundo de acordo com a mudança no input
+corInput.addEventListener('change', ()=> {
+    root.style.setProperty('--variavelBackground', corInput.value)
 })
+
+//mudando blur de acordo com mudança no input
+nivelBlur.addEventListener('change', ()=> {
+    root.style.setProperty('--img-blur', (nivelBlur.value)/10+'px')
+})
+
+//mudando width de acordo com mudança no input
+nivelSize.addEventListener('change', ()=> {
+    root.style.setProperty('--img-width', nivelSize.value+'%')
+})
+
+
+
+
 
